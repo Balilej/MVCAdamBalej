3c3e6acdaadb0c7fe9f2c2e518eb3b6a932f08c8
Bastijin <94435678+Balilej@users.noreply.github.com>
2024-07-12 00:45:11 +0200

    CalcIndexPageSet

---
+++ Controllers/CalcHistrController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCAdamBalej.Services;
+
+namespace MVCAdamBalej.Controllers
+{
+    public class CalcHistrController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public CalcHistrController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var calcDatas = context.CalcDatas.ToList();
+            return View(calcDatas);
+        }
+    }
+}
